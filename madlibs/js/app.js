5e7ed30ae36d65c7f4ac6d34c93801678f392b3b
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2014-05-14 13:19:27 -0500

    lesson 2 assignment 7 done.

---
+++ madlibs/js/app.js
@@ -0,0 +1,54 @@
+angular.module('madlibs',[])
+.controller('myCtrl',function($scope){
+	var words = [
+		{
+			id:"male_name",
+			value:"",
+			placeholder:"male name"
+		},{
+			id:"dirty_task",
+			value:"",
+			placeholder:"dirty task"
+		},{
+			id:"obnoxious_celebrity",
+			value:"",
+			placeholder:"obnoxious celebrity"
+		},{
+			id:"job_title",
+			value:"",
+			placeholder:"Job title"
+		},{
+			id:"celebrity",
+			value:"",
+			placeholder:"celebrity"
+		},{
+			id:"huge_number",
+			value:"",
+			placeholder:"huge number"
+		},{
+			id:"useless_skill",
+			value:"",
+			placeholder:"useless skill"
+		},{
+			id:"tedious_task",
+			value:"",
+			placeholder:"tedious task"
+		},{
+			type:"adjective",
+			value:"",
+			placeholder:"adjective"
+		}];
+	$scope.words = words;
+	$scope.getWord = function(type){
+		var i;
+		for(i=0;i<words.length;i++){
+			if(words[i].id==type){
+				if(words[i].value==''){
+					return words[i].placeholder;
+				}
+				return words[i].value;
+			}
+		}	
+		return '';
+	}
+});
